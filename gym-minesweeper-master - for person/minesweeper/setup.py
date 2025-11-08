baf8f9d69428e109860176935856e0485c658ea8
Sophia Thompson <119273210+smthom1@users.noreply.github.com>
2025-11-08 11:39:03 -0800

    Add files via upload

---
+++ gym-minesweeper-master - for person/minesweeper/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='minesweeper-env',
+    version='0.1.0',
+    
+    install_requires=[
+        'numpy',
+        'gymnasium',
+        'pygame',
+        'six'
+    ]
+)
